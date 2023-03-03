e6e6ebcb61ae9a5bb4b3cbb42558693e4b921897
Anastasiya Pomogaeva <pomogaeva.anasyasiya@gmail.com>
2023-03-03 19:10:15 +0300

    HashSet words extracted from hasWord(String word) method

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -4,13 +4,14 @@ import java.util.Set;
 
 public class WordsChecker {
     protected String text;
+    protected Set<String> words;
 
     public WordsChecker(String text) {
         this.text = text;
+        this.words = new HashSet<>(Arrays.asList(text.split("\\P{IsAlphabetic}+")));
     }
 
     public boolean hasWord(String word) {
-        Set<String> words = new HashSet<>(Arrays.asList(text.split("\\P{IsAlphabetic}+")));
         return words.contains(word);
     }
 }
